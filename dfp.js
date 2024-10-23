cd2974b5326f3dd6a9498cd4c2702dc4ff99aee7
zoelittleada <zoe.little@ada.ac.uk>
2024-10-23 11:01:22 +0000

    some errors

--- dfp.js
+++ dfp.js
@@ -3,15 +3,17 @@ const fs = require('fs');
 function parseFile (indata, outdata, delimiter = ';') {
   if (!fs.existsSync(indata)) return -1;
 
-  count = 0
-
   try{
     fs.writeFileSync(outdata, '')
+
     const data = fs.readFileSync(indata, "utf-8");
     const lines = data.split(/\n/);
+
+    let count = 0
+    
     for (let i = 1; i < lines.length; i++) {
       const line = lines[i].trim();
-      if (line == '') continue;
+      if (!line) continue;
       const [index0, index1] = line.split(delimiter).map(item => item.trim());
       const reviews = index0.substring(0,20);
       //console.log(reviews)
