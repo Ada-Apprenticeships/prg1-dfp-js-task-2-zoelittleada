919dd58422efd7f92d36dd8ab709b6507879a7eb
zoelittleada <zoe.little@ada.ac.uk>
2024-10-23 10:57:21 +0000

    passed tests

--- dfp.js
+++ dfp.js
@@ -1,14 +1,32 @@
 const fs = require('fs');
 
 function parseFile (indata, outdata, delimiter = ';') {
+  if (!fs.existsSync(indata)) return -1;
 
+  count = 0
+
+  try{
+    fs.writeFileSync(outdata, '')
+    const data = fs.readFileSync(indata, "utf-8");
+    const lines = data.split(/\n/);
+    for (let i = 1; i < lines.length; i++) {
+      const line = lines[i].trim();
+      if (line == '') continue;
+      const [index0, index1] = line.split(delimiter).map(item => item.trim());
+      const reviews = index0.substring(0,20);
+      //console.log(reviews)
+      fs.appendFileSync(outdata, `${index1}${delimiter}${reviews}\n`, 'utf-8');
+      count ++;
+    }
+
+    }catch (err) {
+      console.error('Error parsing file:', err);
+      return -1;
+    }
+    return count;
 }
-
-
-
   
-
-
+parseFile('./datafile.csv', './outputfile.csv')
 
 // Leave this code here for the automated tests
 module.exports = {
