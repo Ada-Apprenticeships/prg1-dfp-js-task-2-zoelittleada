b029e34f03ebcf63bbdc3711466f428a60ae6d26
zoelittleada <zoe.little@ada.ac.uk>
2024-10-23 11:23:30 +0000

    all comments

--- dfp.js
+++ dfp.js
@@ -3,27 +3,30 @@ const fs = require('fs');
 function parseFile (indata, outdata, delimiter = ';') {
   if (!fs.existsSync(indata)) return -1; //checks if indata file exists
 
+  //try..catch handles errors so the code doesn't crash
   try{
     fs.writeFileSync(outdata, '') //clears or creates outdata file
 
     const data = fs.readFileSync(indata, "utf-8");
     const lines = data.split(/\n/);
 
-    count = 0 //counts the number of lines processed
+    count = 0 //counts the number of lines processed to return at the end 
     
+    //iterates through lines skipping the first one as this contains the headers 
     for (let i = 1; i < lines.length; i++) {
       const line = lines[i].trim();
-      if (!line) continue; //this skips any empty lines in the file
+      if (!line) continue; //skips any empty lines in file
       
       //trims and splits lines so there are no white spaces and reviews are only 20 characters 
       const [review, sentiment] = line.split(delimiter).map(item => item.trim());
       const shortReview = review.substring(0,20);
       
+      //appends sentiment and shortened review to outdata file in correct format 
       fs.appendFileSync(outdata, `${sentiment}${delimiter}${shortReview}\n`, 'utf-8');
       count ++;
     }
 
-    }catch (err) {
+    }catch (err) { 
       console.error('Error parsing file:', err);
       return -1;
     }
